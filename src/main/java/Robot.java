7c67a9465e68e6cbf3463b7cc28b8d0bf8455e36
Larry Liger <me@ligerbots.org>
2020-01-31 18:51:18 -0500

    one more file

--- src/main/java/Robot.java
+++ src/main/java/Robot.java
@@ -8,6 +8,7 @@
 /*----------------------------------------------------------------------------*/
 
 
+package frc.robot;
 
 import edu.wpi.first.wpilibj.TimedRobot;
 import edu.wpi.first.wpilibj.smartdashboard.SendableChooser;
